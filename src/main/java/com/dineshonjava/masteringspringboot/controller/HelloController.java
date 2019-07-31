7a4b5fe784f527c6de9cd87058b4e1e59dc5eff3
vxuandai <vxuandai@gmail.com>
2019-07-31 22:17:31 +0700

    add hello world spring boot cli

---
+++ src/main/java/com/dineshonjava/masteringspringboot/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.dineshonjava.masteringspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {   
+	@GetMapping("/hello")   
+	String sayHello(){         
+		return "Hello World!!!";   
+	}
+} 
