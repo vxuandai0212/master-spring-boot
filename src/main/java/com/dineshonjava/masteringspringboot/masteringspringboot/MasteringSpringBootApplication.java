aabff92c9be3c9ee65241bf3f658c017bafbb56b
vxuandai <vxuandai@gmail.com>
2019-07-31 20:53:57 +0700

    init project

---
+++ src/main/java/com/dineshonjava/masteringspringboot/masteringspringboot/MasteringSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dineshonjava.masteringspringboot.masteringspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringBootApplication.class, args);
+	}
+
+}
